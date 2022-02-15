7351b9890b63af94fcb0f3acb407614bb9bf8449
OGDobb <73088984+OGDobb@users.noreply.github.com>
2022-02-14 21:04:13 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,66 +1,74 @@
-
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
-var bobObject1, bobObject2, bobObject3, bobObject4, bobObject5, roofObject;
+var engine, world;
+var box1, pig1;
+var backgroundImg,platform;
+var log6
 
-var rope1, rope2, rope3, rope4, rope5;
-// function preload()
-// {
-	
-// }
-
-function setup() {
-	createCanvas(800, 700);
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	//Create the Bodies Here.
-	roofObject = new Roof(400,650,400,50);
-	
-	bobObject1 = new BobObject(280,450,50);
-	bobObject2 = new BobObject(340,450,50);
-	bobObject3 = new BobObject(400,450,50);
-	bobObject4 = new BobObject(460,450,50);
-	bobObject5 = new BobObject(520,450,50);
-
-	rope1=new Rope(bobObject1.body,roofObject.body,-bobDiamater*2,0);
-	rope2=new Rope(bobObject2.body,roofObject.body,-bobDiamater*2,0);
-	rope3=new Rope(bobObject3.body,roofObject.body,-bobDiamater*2,0);
-	rope4=new Rope(bobObject4.body,roofObject.body,-bobDiamater*2,0);
-	rope5=new Rope(bobObject5.body,roofObject.body,-bobDiamater*2,0);
-
-	Engine.run(engine);
-  
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
 }
 
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+    log6 = new Log(230,180,80,PI/2);
+    
 
-function draw() {
-  rectMode(CENTER);
-  background(255,255,0);
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+    platform=new Ground(150,305,300,170);
+    bird = new Bird(100,100);
+
+
    
-  roof.display();
+    chain = new Chain(bird.body,log6.body);
 
-  bobObject1.display();
-  bobObject2.display();
-  bobObject3.display();
-  bobObject4.display();
-  bobObject5.display();
-  
-  rope1.display();
-  rope2.display();
-  rope3.display();
-  rope4.display();
-  rope5.display();
-
-  //console.log(roof);
-  drawSprites();
- 
 }
 
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
 
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
 
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+
+    log6.display();
+    chain.display();
+}
